a34cae970b4e5724f533dd0828f1c2705c1259c1
Yana-Denisova <ladymacbeth@list.ru>
2022-04-23 17:27:30 +0300

    first review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.82.211',
-                 'denisova_web_1',
-                ]
+                 'denisova_web_1', ]
 
 
 # Application definition
